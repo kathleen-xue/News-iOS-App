604f2cecdcfa894c57d38c45f39d46d207dcda2f
Kathleen Xue <kxue@usc.edu>
2020-04-13 17:35:51 -0700

    Initial Commit

---
+++ hw9-private/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  hw9-private
+//
+//  Created by Kathleen Xue on 4/13/20.
+//  Copyright © 2020 Kathleen Xue. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hw9-private/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  hw9-private
+//
+//  Created by Kathleen Xue on 4/13/20.
+//  Copyright © 2020 Kathleen Xue. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
