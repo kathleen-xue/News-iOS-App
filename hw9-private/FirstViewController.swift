54cb07a5741101026e2f86a2f59bd47af2a42a03
Kathleen Xue <kxue@usc.edu>
2020-04-17 22:36:14 -0700

    i rlly rlly dont like swift :/ :(

--- hw9-private/FirstViewController.swift
+++ hw9-private/FirstViewController.swift
@@ -9,14 +9,21 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var weatherApiImg: UIImageView!
+    @IBOutlet weak var weatherApiCity: UILabel!
+    @IBOutlet weak var weatherApiState: UILabel!
+    @IBOutlet weak var weatherApiTemp: UILabel!
+    @IBOutlet weak var weatherApiFeat: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let weather = WeatherGetter()
         weather.getWeather(city:"Palo%20Alto")
     }
-
+    //MARK: Actions
 
 }
 
