407776234baf197efe7a1f04b8900dc089f30b56
Kathleen Xue <kxue@usc.edu>
2020-04-26 02:26:09 -0700

    started getting search results http

---
+++ hw9-private/SearchResultsGetter.swift
@@ -0,0 +1,28 @@
+//
+//  SearchResultsGetter.swift
+//  hw9-private
+//
+//  Created by Kathleen Xue on 4/26/20.
+//  Copyright © 2020 Kathleen Xue. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+class SearchResultsGetter {
+    var data = [Any]()
+    func getSearchResults(query: String, completion: @escaping (Array<Any>) -> Void) -> Void {
+        let searchUrl =  "http://kxue-nodejs.us-east-1.elasticbeanstalk.com/searchResults?query=\(query)"
+        Alamofire.request(searchUrl)
+            .responseJSON{(responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                if let ret_ = swiftyJsonVar["guardianData"].array {
+                    self.data = ret_
+                    completion(self.data)
+                }
+            }
+        }
+    }
+}
--- hw9-private/SearchResultsPageController.swift
+++ hw9-private/SearchResultsPageController.swift
@@ -8,21 +8,15 @@
 
 import Foundation
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class SearchResultsPageController : UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "searchResultsCell", for: indexPath) as! SearchResultsTableCell
-        return cell
-    }
-    
 
     @IBOutlet weak var header: UILabel!
     @IBOutlet weak var searchResultsTable: UITableView!
     var searchQuery = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchResultsTable.dataSource = self
@@ -30,12 +24,22 @@ class SearchResultsPageController : UIViewController, UITableViewDelegate, UITab
         searchResultsTable.reloadData()
         print("in searchresults page controller" )
         print(searchQuery)
+        searchUrl = searchUrl + searchQuery
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         searchResultsTable.reloadData()
         self.header.text = "Search Results"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "searchResultsCell", for: indexPath) as! SearchResultsTableCell
+        return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
